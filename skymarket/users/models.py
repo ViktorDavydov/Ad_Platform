1f4b3b2612989d4493d17df38ba350a7a3347db7
Viktor Davydov <davydov.viktor.andreevich@gmail.com>
2024-03-17 23:33:05 +0300

    user auth process commit

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -10,29 +10,24 @@ NULLABLE = {
 }
 
 
-class UserRoles:
-    ADMIN = "admin"
-    USER = "user"
+class UserRoles(models.TextChoices):
+    ADMIN = "Admin"
+    USER = "User"
 
 
-roles_choices = (
-    (UserRoles.ADMIN, "Admin"),
-    (UserRoles.USER, "User"),
-)
-
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
+    username = None
     first_name = models.CharField(max_length=50, verbose_name='имя пользователя')
     last_name = models.CharField(max_length=50, verbose_name='фамилия пользователя')
     phone = models.CharField(max_length=15, verbose_name='телефон пользователя')
     email = models.EmailField(unique=True, verbose_name="почта пользователя")
-    role = models.CharField(max_length=10, default=UserRoles.USER, choices=roles_choices, verbose_name="роли")
+    role = models.CharField(max_length=10, default=UserRoles.USER, choices=UserRoles.choices, verbose_name="роли")
     image = models.ImageField(upload_to="profile_images", verbose_name="фото пользователя", **NULLABLE)
     is_active = models.BooleanField(default=False, verbose_name="активность пользователя")
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
 
     @property
     def is_admin(self):
@@ -42,18 +37,16 @@ class User(AbstractBaseUser):
     def is_user(self):
         return self.role == UserRoles.USER
 
-    @property
-    def is_superuser(self):
-        return self.is_admin
+    # @property
+    # def is_superuser(self):
+    #     return self.is_admin
 
-    @property
-    def is_staff(self):
-        return self.is_admin
+    # @property
+    # def is_staff(self):
+    #     return self.is_admin
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
     def has_module_perms(self, app_label):
         return self.is_admin
-
-
