6ec8b0fb3c8de3296e5c019a5e2d1bcbf9286862
Viktor Davydov <davydov.viktor.andreevich@gmail.com>
2024-03-19 21:53:50 +0300

    ads models commit

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -5,7 +5,6 @@ class AdPagination(pagination.PageNumberPagination):
     pass
 
 
-# TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
 class AdViewSet(viewsets.ModelViewSet):
     pass
 
